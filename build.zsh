041bf9db3428f652081b4661c87ea810b2b100bd
tophed <chriswacker228@gmail.com>
2025-04-25 19:19:38 -0700

    add mac build script and config

---
+++ build.zsh
@@ -0,0 +1,8 @@
+clang++ -std=c++17 -stdlib=libc++ -target arm64-apple-macos \
+    -I$VULKAN_SDK/include \
+    -I/opt/homebrew/include \
+    -L$VULKAN_SDK/lib \
+    -L/opt/homebrew/lib \
+    src/main.cpp -o build/main \
+    -lvulkan.1 -lglfw \
+    -framework Cocoa -framework QuartzCore -framework Metal -framework IOKit
