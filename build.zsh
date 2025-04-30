04508b4db7636dd7fe1adf997e1c36ac74d69dda
tophed <chriswacker228@gmail.com>
2025-04-30 13:25:40 -0700

    layout transitions

--- build.zsh
+++ build.zsh
@@ -1,9 +1,10 @@
 glslc shaders/shader.frag -o shaders/shader.frag.spv
 glslc shaders/shader.vert -o shaders/shader.vert.spv
 
-clang++ -std=c++17 -stdlib=libc++ -target arm64-apple-macos \
+clang++ -g -std=c++17 -stdlib=libc++ -target arm64-apple-macos \
     -I$VULKAN_SDK/include \
     -I/opt/homebrew/include \
+    -I/Users/chris/include \
     -L$VULKAN_SDK/lib \
     -L/opt/homebrew/lib \
     src/main.cpp -o build/main \
